3e070c2c5cc5e4d1348b03da0f4c23e9a406ee5e
PRAKHAR TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2023-11-24 12:30:39 +0530

    updates

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,35 +2,76 @@ import React, { useState } from 'react';
 import { medicineData } from './varible';
 import dayjs from 'dayjs';
 import _ from 'lodash';
-import { FaSun, FaMoon, FaCoffee } from 'react-icons/fa';
+import { FaSun, FaMoon, FaCloudSun } from 'react-icons/fa';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
 
 
+function icons(slotTime) {
+  let timeA = slotTime.split(":");
 
+  const hour = timeA[0];
+  const time = slotTime.split(" ")
+
+  if (time[1] === "am") {
+    if (hour >= 6 && hour < 12) {
+      return <FaSun />;
+    }
+  }
+  if (time[1] === "pm") {
+    if (hour >= 6 && hour < 12) {
+      return <FaMoon />;
+    } else {
+      return <FaCloudSun />
+    }
+  }
+
+}
 
 function App() {
   const sameDateData = _.groupBy(medicineData, 'created_at');
   const items = Object.entries(sameDateData);
-  const [details, setDetails] = useState()
+  const todayDate = dayjs().format('DD-MM-YYYY');
   let data = "";
 
+  const todayItem = items.find(([date, itemsArray]) => {
+    if (dayjs.utc(date).format('DD-MM-YYYY') === todayDate) {
+      return itemsArray;
+    }
+  });
+  const [details, setDetails] = useState(todayItem && todayItem[1]);
+  const [selectedDate, setSelectedDate] = useState(dayjs().format("DD"));
   if (details) {
     const sameTime = _.groupBy(details, 'slot_time');
-    data = Object.entries(sameTime);
+    const medicineArray = Object.entries(sameTime);
+
+    const sortedMedicineArray = _.sortBy(medicineArray, ([time]) => {
+      const hour = parseInt(time.split(':')[0], 10);
+      const isPm = time.includes('pm');
+      const timeFormat = isPm ? hour + 12 : hour;
+
+      return timeFormat * 100 + parseInt(time.split(':')[1], 10);
+    });
+    const sortedMedicineData = Object.fromEntries(sortedMedicineArray);
+    data = Object.entries(sortedMedicineData);
   }
-  
-  // console.log(data)
+
   return (
     <>
       <div className='flex justify-between flex-wrap'>
         {items.map(([date, itemsArray]) => {
-          const formattedDate = dayjs(date).format('DD');
-          const dayOfWeek = dayjs(date).locale('en').format('ddd');
-
+          const formattedDate = dayjs.utc(date).format('DD');
+          const dayOfWeek = dayjs.utc(date).locale('en').format('ddd');
+          const valid = dayjs().isBefore(dayjs.utc(date).format('YYYY-MM-DD'));
           return (
-            <div key={formattedDate} className={`m-2 border-2 h-24 w-20 max-w-xs overflow-wrap rounded-md cursor-pointer p-2 `}
-              onClick={() => setDetails(itemsArray)}>
+            <div key={formattedDate} className={`m-2 border-2 h-24 w-20 max-w-xs overflow-wrap rounded-md cursor-pointer p-2 ${selectedDate === formattedDate ? 'bg-blue-300' : ''}`}
+              onClick={() => { setDetails(itemsArray); setSelectedDate(formattedDate) }}>
               <p>{formattedDate}</p>
               <p>{dayOfWeek}</p>
+              {
+                !valid && <p className='h-2 w-2 bg-red-600 rounded-xl'></p>
+              }
 
             </div>
           );
@@ -45,13 +86,13 @@ function App() {
                 <div>
                   {
                     slotItems.map((item) =>
-                      <div>
-                        <p key={item.id}>{item.medicine_name}</p>
+                      <div key={item.id} >
+                        <p >{item.medicine_name}</p>
                       </div>
                     )
                   }
                 </div>
-                <p className='text-2xl font-bold'>{slotTime}</p>
+                <p className='text-2xl font-bold'>{icons(slotTime)} {slotTime}</p>
               </div>
             ))}
           </div>
